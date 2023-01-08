f05ce463633c14f34e490fb471745d91cf02080b
Heron <heron.brito@orama.com.br>
2023-01-07 21:17:03 -0300

    client.py return mal identado

--- pytrustnfe/client.py
+++ pytrustnfe/client.py
@@ -26,6 +26,7 @@ def get_authenticated_client(base_url, cert, key):
         print("ERROR: Falha na conexão utilizando o certificado digital e senha infomados. Verifique a validade do certificado")
         exit()
 
+    # Heron - corrigido
     return suds.client.Client(
         base_url, cache=cache, transport=suds_requests.RequestsTransport(session)
     )
