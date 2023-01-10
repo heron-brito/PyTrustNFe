e550b1945d6db6ea9bdd95fc076794e9b502992a
Heron <heron.brito@orama.com.br>
2023-01-10 12:22:14 -0300

    assinatura do elemento xml na nota carioca

--- pytrustnfe/nfse/carioca/__init__.py
+++ pytrustnfe/nfse/carioca/__init__.py
@@ -83,8 +83,9 @@ def _send(certificado, method, **kwargs):
         # sign_tag(certificado, **kwargs)
 
     # xml_send = kwargs["xml"].decode("utf-8")
+    xml_send = kwargs["xml"]
     logger.warning('_send xml_send')
-    # logger.warning(xml_send)
+    logger.warning(xml_send)
     cert, key = extract_cert_and_key_from_pfx(certificado.pfx, certificado.password)
     cert, key = save_cert_key(cert, key)
     logger.warning(f'cert:{cert} key:{key}')
