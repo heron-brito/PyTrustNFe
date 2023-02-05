b2cdf8ba706c8e87a06b3700cb42c6a07e89153e
Heron <heron.brito@orama.com.br>
2023-02-05 16:53:10 -0300

    teste pfx = crypto.load_pkcs12(pfx, password.encode(utf-8)).get_certificate()

--- pytrustnfe/nfse/carioca/__init__.py
+++ pytrustnfe/nfse/carioca/__init__.py
@@ -41,6 +41,7 @@ def _render(certificado, method, **kwargs):
         reference = "Cancelamento_NF%s" % kwargs["cancelamento"]["numero_nfse"]
 
     logger.warning('xml_send assinador')
+    logger.warning(certificado.__dict__)
     logger.warning(f'{certificado.pfx},pass:  {certificado.password}')
     signer = Assinatura(certificado.pfx, certificado.password)
     # xml_send = signer.assina_xml(xml_send, reference)
